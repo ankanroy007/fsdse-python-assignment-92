8c25637fb453ec3c3318d0ec0cf176d11786dc04
ankanroy007 <ankanroy007@users.noreply.github.com>
2017-06-06 15:12:05 +0000

    Done

--- build.py
+++ build.py
@@ -6,3 +6,17 @@ def solution(data, index):
     """
     Enter your code here
     """
+    df = pd.DataFrame(data= data,index= index)
+    return df
+
+
+exam_data = {
+'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']
+}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+solution(exam_data,labels)
